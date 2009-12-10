9fc3a0248ec5aefeaa3dcdd24e3b410221675044
Andy Herrman <herms@ewing.local>
2009-12-10 16:01:39 -0500

    refined Greed score calculation ode

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -30,51 +30,31 @@ require 'edgecase'
 # Your goal is to write the score method.
 
 def score(dice)
-  counts = count_dice_types(dice)
-
-  score = 0
-
-  counts.each_pair { |value, count|
-    score += score_dice_value(value, count)
+  count_dice_types(dice).each_pair.inject(0) { |score, pair|
+    value, count = pair
+    score + score_dice_value(value, count)
   }
-
-  score
 end
 
 def score_dice_value(value, count)
-  if count == 0
-    0
-  elsif count > 2
-    tripple_score(value) + score_dice_value(value, count - 3)
-  elsif value == 1
-    100 * count
-  elsif value == 5
-    50 * count
-  else
-    0
-  end
+  return 0 if count == 0
+  return tripple_score(value) + score_dice_value(value, count - 3) if count > 2
+  return 100 * count if value == 1
+  return 50 * count if value == 5
+  0
 end
 
 def tripple_score(value)
-  if value == 1
-    1000
-  else
-    value * 100
-  end
+  return 1000 if value == 1
+  value * 100
 end
 
 def count_dice_types(dice)
-  counts = {}
-
-  dice.each { |item|
-    if counts[item].nil?
-      counts[item] = 1 
-    else
-      counts[item] += 1
-    end
+  dice.inject({}) { |counts, value|
+    counts[value] = 0 if counts[value].nil?
+    counts[value] += 1
+    counts
   }
-  
-  counts
 end
 
 class AboutScoringAssignment < EdgeCase::Koan
